bc85dec98e070ddf6e0b10fe72d54ee3edcb1a98
RicardoOliveiraFisica <ric_fisica@yahoo.com.br>
2023-11-21 23:18:35 -0300

    final version

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,41 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import model.entities.Account;
+import model.exceptions.DomainException;
+
 public class Program {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Locale.setDefault(Locale.US);
 		
+		try {
+			System.out.println("Enter account data");
+			System.out.print("Number: ");
+			int number = sc.nextInt();
+			sc.nextLine();
+			System.out.print("Holder: ");
+			String holder = sc.nextLine();
+			System.out.print("Initial balance: ");
+			Double balance = sc.nextDouble();
+			System.out.print("Withdraw limit: ");
+			Double withdrawLimit = sc.nextDouble();
+			
+			Account account = new Account(number, holder, balance, withdrawLimit);
+			
+			System.out.print("Enter amount for withdraw: ");
+			account.withdraw(sc.nextDouble());
+			System.out.println("New balance: " + String.format("%.2f", account.getBalance()));
+			
+		} catch (DomainException e) {
+			System.out.println("Withdraw error: " + e.getMessage());
+		} catch (RuntimeException e) {
+			System.out.println("Unexpected error");
+		}
+		sc.close();
 
 	}
 
